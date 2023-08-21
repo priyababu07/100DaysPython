1fbfc0acd85217fa980081144c16d2b848e27796
priyababu07 <priyababu312@gmail.com>
2023-08-21 22:48:51 +0530

    code6

--- Day5/codeChallenge5.py
+++ Day5/codeChallenge5.py
@@ -6,21 +6,21 @@ numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 characters = ['@', '#', '$', '%', '^', '&', '*']
 
 length = int(input("The length of the password: "))
-char_count = int(input("The number of letters required: "))
-num_count = int(input("The number of numbers required: "))
-chara_count = int(input("The number of characters required: "))
+char = int(input("The number of letters required: "))
+num = int(input("The number of numbers required: "))
+chara = int(input("The number of characters required: "))
 
-for j in range(char_count):
+for j in range(char):
     p += random.choice(letters)
     
-for j in range(num_count):
+for j in range(num):
     p += random.choice(numbers)
     
-for j in range(chara_count):
+for j in range(chara):
     p += random.choice(characters)
 
 
-remaining_length = length - (char_count + num_count + chara_count)
+remaining_length = length - (char + num + chara)
 for j in range(remaining_length):
     p += random.choice(letters + numbers + characters)
 
---
+++ Day6/UnderstandingFunction.py
@@ -0,0 +1,8 @@
+def sum():
+    num1=int(input("Enter the first number"))
+    num2=int(input("Enter the second number"))
+    sum = num1+num2
+    return sum
+    
+k=sum()
+print(k)
